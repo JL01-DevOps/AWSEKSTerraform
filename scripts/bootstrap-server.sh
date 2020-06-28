39b0e4c99a077b574072909b4d1eddabf02e6ed5
Christos Sarakasidis <csarakasidis@christoss-mbp.home>
2020-06-28 18:50:59 +0100

    scripts server added

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -1,25 +1,27 @@
+#!/bin/sh
+
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 
-# sudo chmod +x ./kubectl
-# 
-# sudo mv ./kubectl /usr/local/bin/kubectl
-# 
+sudo chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 # vim ~/.kube/config
-# 
-# curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator
-# 
-# chmod +x ./aws-iam-authenticator
-# 
-# sudo mv aws-iam-authenticator /usr/local/bin
-# 
-# aws-iam-authenticator version
-# 
+
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/aws-iam-authenticator
+
+chmod +x ./aws-iam-authenticator
+
+sudo mv aws-iam-authenticator /usr/local/bin
+
+aws-iam-authenticator version
+
 # vim ~/.aws/credentials
-# 
-# curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
-# 
-# chmod 700 get_helm.sh
-# 
-# ./get_helm.sh
-# 
-# helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+
+chmod 700 get_helm.sh
+
+./get_helm.sh
+
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
