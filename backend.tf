64ddbc8a6814a0d9baedb8fd60c4ae945df3be6e
Jack Longmuir <jlongmuir@Mainframe.local>
2020-08-03 15:29:07 +0100

    Initial commit of AWSEKSTerraform.

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "csarakasidis-terraform-remote-state-storage-s3"
+    bucket = "jlongmuir-terraform-remote-state-storage-s3"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
