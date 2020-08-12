13206136230a725d62a2aa0655a53b7897404113
Jack Longmuir <jlongmuir@Mainframe.local>
2020-08-12 15:06:37 +0100

    Backend was changed.

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jlongmuir-terraform-remote-state-storage-s3-oregon"
-    key    = "terraform-eks.tfstate"
+    key    = "terraform.tfstate"
     region = "us-west-2"
   }
 }
